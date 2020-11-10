5a01973e14c30717e4dabe1f7f2df142800bfdd1
Jiweon Park <jiweon.park@tell.com>
2020-11-10 00:07:53 -0800

    added bootstrapcss && guesswords test

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -9,6 +9,29 @@ const GuessWords = (props) => {
                 Try to guess the secret word!
             </span>
         )
+    } else {
+        const guessWordsRows = props.guessedWords.map((word, i) => (
+            <tr data-test="guessed-word" key={i}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+        contents = (
+            <span data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table className="table table-sm">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {guessWordsRows}
+                    </tbody>
+                </table>
+            </span>
+        )
     }
     return (
         <div data-test="component-guessed-words">
